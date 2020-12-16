ba70d6b52997c4948d22a78cddaf6bef1cf4680f
kate <ketrina113@gmail.com>
2020-12-16 11:00:33 +0200

    routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,24 +2,44 @@
 
 use App\Task;
 use Illuminate\Http\Request;
+Route::group(['prefix'=>'tasks'], function (){
+    /**
+     * Вывести панель с задачами
+     */
+    Route::get('/', function () {
+        $tasks = Task::orderBy('created_at', 'asc')->get();
 
-/**
- * Вывести панель с задачами
- */
-Route::get('/tasks', function () {
-   return view('tasks.index');
+        return view('tasks.index', [
+            'tasks'=>$tasks
+        ]);
+    })->name('tasks.all');
+
+    /**
+     * Добавить новую задачу
+     */
+    Route::post('/', function (Request $request) {
+        $validator = Validator::make($request->all(),
+            ['name'=>'required|max:255',
+            ]);
+            if($validator->fails()){
+                return redirect(route('tasks.all'))
+                    ->withInput()
+                    ->withErrors($validator); //для вывода ошибок
+            }
+        $name = $request->name;
+        $task = new Task();
+        $task->name = $name;
+        $task->save();
+        return redirect(route('tasks.all'));
+    })->name('tasks.add');
+
+    /**
+     * Удалить задачу
+     */
+    Route::delete('/{task}', function (Task $task) {
+        $task->delete();
+        return redirect(route('tasks.all'));
+    })->name('tasks.delete');
 });
 
-/**
- * Добавить новую задачу
- */
-Route::post('/tasks', function (Request $request) {
-    //
-});
 
-/**
- * Удалить задачу
- */
-Route::delete('/tasks/{task}', function (Task $task) {
-    //
-});
